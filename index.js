9a98c8bea1c7f8d676ea7f09a845ff7ffd157ed3
eniallator <eniallatormc@gmail.com>
2019-05-16 10:34:19 +0100

    Added an input for the number of segments. Made the main loop more elegant and dynamic

--- index.js
+++ index.js
@@ -1,32 +1,54 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
-const numSegments = 20
+const segInput = document.getElementById('segments')
+const segError = document.getElementById('num-seg-error')
+
+const defaultNumSegments = 20
+let numSegments = defaultNumSegments
+
 const speed = 1
 const maxSegmentWidth = 20
 const ageOfDeath = 200
-const segLength = (2 * Math.PI) / numSegments
+let progress = 0
 
 const curve = x => (2 * x - 1) ** 5 / 2 + 0.5
-const segments = new Array(numSegments).fill().map((_, i) => (i / numSegments) * ageOfDeath)
+
+segInput.addEventListener('input', () => {
+    if (segInput.value === '') {
+        segError.classList.add('hidden')
+        numSegments = defaultNumSegments
+        return
+    }
+    const newNumSegments = Number(segInput.value)
+    if (isNaN(newNumSegments) || newNumSegments % 1 !== 0 || newNumSegments < 1 || newNumSegments > 1000) {
+        segError.classList.remove('hidden')
+        numSegments = defaultNumSegments
+    } else {
+        segError.classList.add('hidden')
+        numSegments = newNumSegments
+    }
+})
 
 ctx.fillStyle = 'black'
 ctx.strokeStyle = 'white'
 
 function run() {
     ctx.fillRect(0, 0, canvas.width, canvas.height)
+    const segLength = (2 * Math.PI) / numSegments
 
-    for (let i = 0; i < segments.length; i++) {
-        const donePercent = curve(segments[i] / ageOfDeath)
+    for (let i = 0; i < numSegments; i++) {
+        const currSeg = (progress + (i / numSegments) * ageOfDeath) % ageOfDeath
+        const donePercent = curve(currSeg / ageOfDeath)
         const mag = donePercent * (canvas.width / 2 + 50)
 
         ctx.beginPath()
         ctx.arc(canvas.width / 2, canvas.height / 2, mag, segLength * i, segLength * (i + 1))
         ctx.lineWidth = donePercent * maxSegmentWidth
         ctx.stroke()
-
-        segments[i] = (segments[i] + speed) % ageOfDeath
     }
+
+    progress = (progress + speed) % ageOfDeath
 
     requestAnimationFrame(run)
 }
